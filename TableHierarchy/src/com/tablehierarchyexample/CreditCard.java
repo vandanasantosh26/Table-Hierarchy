e9e4297cf5696210420290708ec0e60bbd3ecc18
Vandana <Vandana@Admin.fios-router.home>
2018-09-26 20:58:46 -0400

    table-per-hierarchy

---
+++ TableHierarchy/src/com/tablehierarchyexample/CreditCard.java
@@ -0,0 +1,15 @@
+package com.tablehierarchyexample;
+
+public class CreditCard extends Payment{
+
+	private String CreditCardType;
+
+	public String getCreditCardType() {
+		return CreditCardType;
+	}
+
+	public void setCreditCardType(String creditCardType) {
+		CreditCardType = creditCardType;
+	}
+
+}
